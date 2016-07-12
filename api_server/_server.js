2512fd879f367f1cfd69c868039509f4925ad27d
KMSkelton <kmskelton@outlook.com>
2016-07-12 09:28:55 -0700

    linted. Travis build should again pass

--- api_server/_server.js
+++ api_server/_server.js
@@ -6,12 +6,9 @@ const trailSeed = require('./lib/trail_seed');
 
 app.use(bodyParser.json());
 
-//const forecastRouter = require(__dirname + '/routes/forecast_routes');
-//app.use('/api', trailPop);
 app.use('/api', trailSeed);
 trailSeed();
 
-//app.use('/api', forecastRouter);
 app.use(express.static(__dirname + '/../client/build'));
 
 app.use((req, res, next) => {
@@ -20,6 +17,8 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, DELETE, PUT, PATCH');
   next();
 });
+
+// the following is for the testing server
 module.exports = exports = {
   server: {
     close: function() {
@@ -30,7 +29,6 @@ module.exports = exports = {
     console.log('mongoString', mongoString);
     console.log('port from _server.js:', port);
     mongoose.connect(mongoString);
-  //  trailPop();
     return this.server = app.listen(port, cb);
   },
   // close function is so tests can close the test server
